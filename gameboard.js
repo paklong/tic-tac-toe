f6edd9a98a0933785f6289dc6ba7764376ec820c
paklong <paklong2556@gmail.com>
2025-01-05 20:49:14 -0800

    Add setCell API from gameboard module

--- gameboard.js
+++ gameboard.js
@@ -1,17 +1,29 @@
 const gameboard = (function () {
 
     const gameboard = [
-        ['O', 'X', 'O'],
-        ['O', 'X', 'O'],
-        ['X', 'X', 'O'],
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
     ];
 
     //cache dom
     const $cells = document.querySelectorAll('.cell');
-    // $cells[0].getAttribute('data-index')
 
-    //render
-    function render(params) {
+    //renders
+    const render = () => $cells.forEach(displayCells);
 
+    const displayCells = function (cell) {
+        const dataIndex = cell.getAttribute('data-index').split(',');
+        cell.innerText = gameboard[dataIndex[0]][dataIndex[1]];
+    };
+
+    const setCells = function (targetX, targetY, mark) {
+        gameboard[targetX][targetY] = mark;
+        render();
     }
+
+    return {
+        setCells
+    };
+
 })();
