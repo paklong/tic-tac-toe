9428ef6a0a2f1aeb423dbc348d5ab99ed89e6275
paklong <paklong2556@gmail.com>
2025-01-05 21:47:28 -0800

    Display message on every click
    Alternate between 2 player's name

--- game.js
+++ game.js
@@ -1,15 +1,14 @@
 (function game() {
-    const player1 = createPlayer('player1', 'x');
-    const player2 = createPlayer('player2', 'o');
+    const player1 = createPlayer('john', 'x');
+    const player2 = createPlayer('Kelvin', 'o');
 
-    const player = {
-        player1: player1,
-        player2: player2
-    };
+    const player = [player1, player2];
 
-    let whoIsTheTurnNow = 'player1';
+    let whoIsTheTurnNow = 0;
+
     //cache dom
     const $cells = document.querySelectorAll('.cell');
+    const $message = document.querySelector('.message');
 
     //listen to click
     $cells.forEach(
@@ -17,11 +16,25 @@
     )
 
     function iAmClicked(e) {
+        updateGameBoard(e);
+        togglePlayer();
+        updateMessage();
+    };
+
+    function updateGameBoard(e) {
         const [targetX, targetY] = [...e.target.getAttribute('data-index').split(',')];
         const mark = player[whoIsTheTurnNow].getMark();
         gameboard.setCells(targetX, targetY, mark);
-        whoIsTheTurnNow = whoIsTheTurnNow === 'player1' ? 'player2' : 'player1';
     };
 
+    function updateMessage() {
+        $message.innerHTML = `${player[whoIsTheTurnNow].getClearName()}'s turn now`;
+    };
+
+    function togglePlayer() {
+        whoIsTheTurnNow = (whoIsTheTurnNow === 0) ? 1 : 0;
+    };
+
+    updateMessage();
 
 })();
--- player.js
+++ player.js
@@ -1,8 +1,9 @@
 const createPlayer = function (name, mark) {
     const getName = () => name;
     const getMark = () => mark.toUpperCase();
+    const getClearName = () => name[0].toUpperCase() + name.slice(1, name.length);
     return {
-        getName, getMark
+        getName, getMark, getClearName
     };
 };
 
