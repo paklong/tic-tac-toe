cd4b1e73d0c888894d24adc28dbe451c6b399741
paklong <paklong2556@gmail.com>
2025-01-05 19:08:45 -0800

    Add barebone gameboard

---
+++ gameboard.js
@@ -0,0 +1,17 @@
+const gameboard = (function () {
+
+    const gameboard = [
+        ['O', 'X', 'O'],
+        ['O', 'X', 'O'],
+        ['X', 'X', 'O'],
+    ];
+
+    //cache dom
+    const $cells = document.querySelectorAll('.cell');
+    // $cells[0].getAttribute('data-index')
+
+    //render
+    function render(params) {
+
+    }
+})();
